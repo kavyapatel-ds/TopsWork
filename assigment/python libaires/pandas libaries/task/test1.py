945ddda250a06d22bdb8fbf3d0dfcdaef20bdd73
kavyapatel-ds <kavyapatel123.4546@gmail.com>
2026-08-07 09:45:15 +0530

    Add files via upload

---
+++ assigment/python libaires/pandas libaries/task/test1.py
@@ -0,0 +1,8 @@
+import pandas as pd
+name=pd.Series(["om","jai","jagdish","mahesh"])
+print(name,type(name))
+print(name.size)
+print(name.shape)
+print(name.ndim)
+print(name.values)
+print(len(name))
